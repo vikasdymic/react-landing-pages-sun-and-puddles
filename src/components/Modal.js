be2eb3a6e96924e006c1e2d8e22da9f79f08eb77
Dymic Digital <tech@dymic.com>
2019-07-03 10:35:27 -0700

    removed display none styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,7 +13,7 @@ class Modal extends Component {
                 <div id='modal'>
                     <h2>Coming Soon!</h2>
                     <p>Thank you for expressing your interest in {this.props.children}! We are putting the finishing touches on {this.props.children} and you'll be the first to know when it's done.</p>
-                    <form style='display: none;' method="POST" action='/' name="emailresponse" data-netlify="true">
+                    <form method="POST" action='/' name="emailresponse" data-netlify="true">
                       <input type="hidden" name="form-name" value="emailresponse" />
                       <input className="input" name='email' type="email" placeholder="email"/>
                       <button className='button small' type='submit' name='button'>Submit</button>
