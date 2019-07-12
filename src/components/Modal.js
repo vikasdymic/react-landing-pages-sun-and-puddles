39e0910ee7fd9bef7d60eba06c3beb3057fb8d86
Dymic Digital <tech@dymic.com>
2019-07-12 13:44:28 -0700

    added google tag manager stuff

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -16,7 +16,7 @@ class Modal extends Component {
                     <form method="POST" action='/' name="emailresponse" data-netlify="true">
                       <input type="hidden" name="form-name" value="emailresponse" />
                       <input className="input" name='email' type="email" placeholder="email"/>
-                      <button className='button small' type='submit' name='button'>Submit</button>
+                      <button className='button small google-tag-button' type='submit' name='button'>Submit</button>
                     </form>
                     <button class='button medium' onClick={this.props.onClose}>Close</button>
                 </div>
